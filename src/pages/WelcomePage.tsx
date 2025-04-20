ca07bc5aa155e5da4f04904dbd203119278de933
miriamsobrino <miriamsobrinodev@gmail.com>
2025-04-20 17:17:14 +0200

    fix image of welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -34,29 +34,23 @@ function WelcomePage() {
         Get Started
       </Button>
       <div
-        className=' w-[90%] lg:w-[60%] h-60 absolute -bottom-10 lg:h-full  lg:relative rounded-md lg:border-t-4 lg:border-r-4 lg:border-l-4 border-4 border-blue-50  mt-8 lg:mt-4 lg:[mask-image:linear-gradient(to_bottom,black_60%,transparent_100%)] 
-'
+        className={` w-[90%] lg:w-[60%] h-60 absolute -bottom-10 lg:h-full shadow-lg ${
+          darkMode ? 'shadow-blue-200' : 'shadow-blue-500/40'
+        } lg:relative rounded-md lg:border-t-4 lg:border-r-4 lg:border-l-4 border-4 border-blue-50  mt-8 lg:mt-4  `}
       >
         {darkMode ? (
           <img
             src='images/tasknow-screenshot-darkmode.webp'
-            alt='Imagen'
-            className='w-full h-full object-cover  object-left lg:object-top z-20 '
+            alt='Captura de la aplicación en modo light'
+            className='w-full mx-auto h-96 object-cover object-left lg:object-top z-20 '
           />
         ) : (
           <img
             src='images/tasknow-screenshot.webp'
-            alt='Imagen'
-            className='w-full mx-auto h-full object-cover object-left lg:object-top z-20 '
+            alt='Captura de la aplicación en modo dark'
+            className='w-full mx-auto h-96 object-cover object-left lg:object-top z-20 '
           />
         )}
-        <div
-          className={`w-full lg:w-[60%] h-56 lg:h-96  ${
-            darkMode
-              ? 'bg-gradient-to-b from-gray-500 via-gray-800 to-gray-900'
-              : 'bg-gradient-to-b from-blue-500/40 via-blue-200 to-blue-50'
-          } blur-md absolute -z-1 inset-0  `}
-        ></div>
       </div>
     </div>
   );
